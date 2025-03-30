8f00e397f39f79f2fd5d2fcf81a6bab1d0b79fc8
RokAnzel <roki.anzel@gmail.com>
2025-03-30 18:59:15 +0200

    filled the function

--- ORVNaloga1.py
+++ ORVNaloga1.py
@@ -12,3 +12,18 @@ dbarvo = True
 List_frame = []
 
 def get_pixel_color(event, x, y, flags, param):
+    global frame,Tls,Brs,drawing,mouse_click
+    if event == cv.EVENT_LBUTTONDOWN:
+        if frame is not None:
+            drawing = True
+            Tls = (x,y)
+    if event == cv.EVENT_MOUSEMOVE:
+        if frame is not None and drawing:
+            Brs = (x,y)
+    if event == cv.EVENT_LBUTTONUP:
+        if frame is not None:
+            drawing = False
+            mouse_click = True
+            Brs = (x,y)
+    if mouse_click:
+        cv.setMouseCallback("Video Stream", lambda *args: None)
