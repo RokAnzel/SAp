f71731e0c144407361ed1fa6296bec163d23b879
RokAnzel <roki.anzel@gmail.com>
2025-03-30 17:40:53 +0200

    Added the ability to select which sort alghorithem to use

--- Vaja1.cpp
+++ Vaja1.cpp
@@ -2,6 +2,41 @@
 #include <vector>
 using namespace std;
 
+bool Branje_Stevil(vector<int>& vec, const char s[]) {
+	ifstream input(s);
+	int st;
+
+	if (!input.is_open()) {
+		return false;
+	}
+
+	while (!input.eof()) {
+		input >> st;
+		vec.push_back(st);
+		while (isspace(input.peek())) input.get();
+	}
+	input.close();
+	return true;
+}
+
+void Izpis_Stevil(int* polje, unsigned int velikost) {
+	ofstream output("out.txt");
+
+	for (int i = 0; i < velikost; i++)
+		output << polje[i] << ' ';
+}
+
 int main(int argc, const char* argv[]) {
-return 0;
+	vector<int> A;
+
+	if (argc < 3) return 0;
+	if (!Branje_Stevil(A, argv[2])) return 0;
+
+	if (argv[1][0] == '0') {
+	}
+	else {
+	}
+	Izpis_Stevil(&A[0], A.size());
+
+	return 0;
 }
